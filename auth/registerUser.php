b8ebdf9c18a0891370f9948e7209a18b2aa8f947
Jose Antonio <jose.antonio1994@outlook.com>
2019-06-06 16:59:51 -0500

    Registro de Usuarios

---
+++ auth/registerUser.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Registro de Usuarios</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="recursos/css/index.css">
+</head>
+<body>
+
+</body>
+</html>
