69bde29b5ce1346f9a65292becc30b1b23528560
Jose Antonio <jose.antonio1994@outlook.com>
2019-06-06 18:27:06 -0500

    Administrador
    Se creó el formulario para el registro de administradores. Solo es la vista.

---
+++ auth/header.php
@@ -0,0 +1,5 @@
+<div class="color-cabecera">
+    <div class="wrap">
+    	<img src="../recursos/img/header.png" alt="SEP">
+    </div>
+</div>
--- auth/registerUser.php
+++ auth/registerUser.php
@@ -10,9 +10,62 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="recursos/css/index.css">
+    <link rel="stylesheet" type="text/css" href="../recursos/css/index.css">
 </head>
 <body>
+ <?php
+ 	include 'header.php';
+	include '../navbar.php';
+ ?>
 
+ <center>
+ 	<h3>Registro de Usuarios</h3>
+ </center>
+ <div class="container">
+ 	<form method="POST" action="">
+ 		<div class="form-group row">
+ 		<label class="col-sm-4">Ingrese un correo electrónico y una contraseña</label>
+ 	</div>
+ 	<div class="form-group row">
+ 		<label class="col-sm-2">Correo electrónico</label>
+ 		<div class="col-md-4">
+            <input id="email" type="email" class="form-control" name="email" required autocomplete="email" autofocus>
+        </div>
+        <div class="checkbox col-sm-4">
+		  <label><input type="checkbox" value="">Acepto todas las condiciones y términos</label>
+		</div>
+ 	</div>
+ 	<div class="form-group row">
+ 		<label class="col-sm-2">Contraseña</label>
+ 		<div class="col-md-4">
+            <input id="password" type="password" class="form-control" name="password" required autocomplete="password" autofocus>
+        </div>
+ 	</div>
+ 	<div class="form-group row">
+ 		<label class="col-sm-2">Confirmar contraseña</label>
+ 		<div class="col-md-4">
+            <input id="password2" type="password" class="form-control" name="password2" required autocomplete="password2" autofocus>
+        </div>
+ 	</div><br>
+ 	<div class="form-group row">
+ 		<label class="col-sm-4">Seleccione el rol:</label>
+ 	</div>
+ 	<div class="form-group row">
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Administrador</label>
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Alumno</label>
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Docente</label>
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Jefe de Departamento</label>
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Administrativo</label>
+ 		
+ 	</div>
+ 	<div class="form-group row">
+ 		<label class="radio-inline col-sm-2"><input type="radio" name="optradio">Empresa</label>
+ 	</div>
+ 	<div class="form-group row">
+ 		<button type="submit" name="regresar" class="btn btn-primary col-sm-1">Regresar</button>
+ 		<button type="submit" name="siguiente" class="btn btn-primary col-sm-1" style="margin-left: 10px;">Siguiente</button>
+ 	</div>
+ 	</form>
+ </div>
 </body>
 </html>
