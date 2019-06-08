9982fa6d7534d4666ed6004166579af012042420
Jose Antonio <jose.antonio1994@outlook.com>
2019-06-08 16:39:41 -0500

    Modelo Administrador
    Se está desarrollando el modelo de Administrador para el registro de usuarios administradores.

--- modelos/Administrador.php
+++ modelos/Administrador.php
@@ -1,5 +1,38 @@
 <?php
 require_once 'Conexion.php';
 
+class Administrador extends Conexion
+{
+	function __construct()
+	{
+		$this->connect_db();
+	}
+
+	/**
+	 *  Método para limpiar las variables antes de poder registrar en la base de datos, esto se hace para evitar posibles inyecciones SQL en la base de datos.
+	 */
+	public function sanitize($var){
+	  $clean = mysqli_real_escape_string($this->con, $var);
+	  return $clean;
+	}
+
+	public function read(){
+		$sql = "SELECT * FROM administrador";
+		$res = mysqli_query($this->con, $sql);
+		return $res;
+	}
+
+	public function insertUser($nombre, $primer_apellido, $segundo_apellido, $curp, $fecha_naci, $domicilio, $colonia, $idestado, $idciudad, $cod_postal, $correo, $telefono, $civil, $sexo, $password, $idroles)
+	{
+		
+	}
+
+	public function insertAdmin($idescolaridad, $rfc, $nombre)
+	{
+		$sql = "INSERT INTO administrador VALUES (NULL, (int)'$idescolaridad','$rfc', (SELECT idusuarios FROM usuarios WHERE nom_usuario = '$nombre'))";
+		$res = mysqli_query($this->con, $sql);
+		return $res;
+	}
+}
 
 ?>
