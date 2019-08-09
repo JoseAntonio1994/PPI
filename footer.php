915d40934b4245c6a69ee9aeee984eef1d2924f6
Jose Antonio <jose.antonio1994@outlook.com>
2019-08-09 15:03:19 -0500

    Cambio de formato de fecha

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="content">
     <h3>------------------------------------------------------------------------------------------------------</h3>
     <h5>Plataforma de Procesos Institucionales | www.ppi.edu.mx</h5>
-    <h5>Instituto Tecnológico de Veracruz - Algunos derechos reservados &copy; 2017</h5>
+    <h5>Instituto Tecnológico de Veracruz - Algunos derechos reservados &copy; <?php echo date('Y'); ?></h5>
     <h5>Calz. Miguel Angel de Quevedo 2779</h5>
     <h5>Col. Formando Hogar, Veracruz, Ver. MÉXICO CP 91860</h5>
     <h5>(229) 934 15 00</h5>
