50dd11e0029954aae905736105c5c3ba15d5bcb7
Eduardo Daniel Martínez Cesta <daniell9404@outlook.com>
2019-08-19 12:27:26 -0500

    unirse

--- tutoriasAlumno/unirse.php
+++ tutoriasAlumno/unirse.php
@@ -22,9 +22,6 @@
 <div class="container-fluid col-sm-12">
   <button type="button" class="list-group-item" data-toggle="modal" data-target="#myModal">Cras justo odio</button>
   <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
-  <button type="button" class="list-group-item">Morbi leo risus</button>
-  <button type="button" class="list-group-item">Porta ac consectetur ac</button>
-  <button type="button" class="list-group-item">Vestibulum at eros</button>
 </div>
 
 <div class="row">
